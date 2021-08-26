970705b3671b55aae1c3c0a14b9e47721391f219
Puneet <puneetvashisht@gmail.com>
2021-08-26 19:28:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>This is my own stuff, see what changes, this is amazing</h2>
+    </div>
+  );
+}
