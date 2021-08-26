32a9d31e4821006e5ef368ada924809882173c16
Puneet <puneetvashisht@gmail.com>
2021-08-26 20:09:55 +0530

    getting posts from fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./styles.css";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [post, setPost] = useState("");
+
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/puneetvashisht/demo-1/courses/4")
+      .then((res) => res.json())
+      .then((data) => setPost(data));
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>
-        This is my own stuff, see what changes, commit this & change again
-      </h2>
+      <h2>{post.title}</h2>
     </div>
   );
 }
